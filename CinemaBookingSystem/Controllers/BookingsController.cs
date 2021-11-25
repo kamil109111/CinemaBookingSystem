72888af9241ea2ff273e876dae7c14ce8d29461d
Kamil Lachowicz <kamil109111@gmail.com>
2021-11-25 23:45:19 +0100

    Add seat check js script in view

--- CinemaBookingSystem/Controllers/BookingsController.cs
+++ CinemaBookingSystem/Controllers/BookingsController.cs
@@ -2,6 +2,7 @@
 using CinemaBookingSystem.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace CinemaBookingSystem.Controllers
         {
             _context = context;
         }
-        public async Task<IActionResult> IndexAsync(int? id)
+        public async Task<IActionResult> Index(int? id)
         {
             List<Ticket> tickets = await _context.Tickets
                 .Where(s => s.SeanceId == id)
@@ -27,9 +28,23 @@ namespace CinemaBookingSystem.Controllers
             ViewData["Title"] = tickets.FirstOrDefault().Seance.Movie.Title.ToString();
             ViewData["Genre"] = tickets.FirstOrDefault().Seance.Movie.Genre.Name.ToString();
             ViewData["RunningTime"] = tickets.FirstOrDefault().Seance.Movie.RunningTime.ToString();
-            ViewData["Language"] = tickets.FirstOrDefault().Seance.Movie.OriginalLanguange.ToString();            
+            ViewData["Language"] = tickets.FirstOrDefault().Seance.Movie.OriginalLanguange.ToString();
+            ViewData["DateTime"] = tickets.FirstOrDefault().Seance.ShowingDate.ToString("dd/MM/yyyy HH:mm");
 
             return View(tickets);
+        }
+
+        public ActionResult HandleButtonClick(string id)
+        {
+            int ticketId = Int32.Parse(id);
+            var ticket = _context.Tickets
+                .Where(t => t.Id == ticketId).SingleOrDefault();
+
+            ticket.IsAvailable = !ticket.IsAvailable;
+            _context.Update(ticket);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", new { id = ticket.SeanceId });
         }
     }
 }
